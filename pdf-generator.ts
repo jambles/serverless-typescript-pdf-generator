8956231b926912db96309901b8617c5fbdb25694
Jason <jtlamble@gmail.com>
2018-01-21 10:14:56 +1100

    skeleton

---
+++ pdf-generator.ts
@@ -0,0 +1,10 @@
+export const generatePdf = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'not yet impld'
+        })
+    };
+
+    callback(null, response);
+};
