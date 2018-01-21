02dc19b438735bd3905e92b794863981d89e9908
Jason <jtlamble@gmail.com>
2018-01-21 17:14:33 +1100

    added working pdf output

--- pdf-generator.ts
+++ pdf-generator.ts
@@ -1,10 +1,39 @@
-export const generatePdf = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'not yet impld'
-        })
-    };
+import * as wkhtmltopdf from "wkhtmltopdf";
+import * as MemoryStream from 'memorystream';
+import * as AWS from 'aws-sdk';
+import * as  fs from 'fs-extra';
+import * as  Buffer from 'buffer';
 
-    callback(null, response);
+const pdfS3Bucket = `${process.env.pdfS3Bucket}-${process.env.ENV}`;
+process.env['PATH'] = process.env['PATH'] + ':/tmp';
+
+export const generatePdf = async (event, context, callback) => {
+    await preparePdfBinary();
+
+    const htmlUtf8 = new Buffer.Buffer('PGJvZHk+SGVsbG8gd29ybGQ8L2JvZHk+', 'base64').toString('utf8');
+
+    convertToPdf(htmlUtf8, function (pdf) {
+        const params = {
+            Bucket: pdfS3Bucket,
+            Key: new Date().getTime() + '-file.pdf',
+            Body: pdf
+        };
+        new AWS.S3().putObject(params, function (err, data) {
+            context.done(null, {pdf_base64: pdf.toString('base64')});
+        })
+    });
 };
+
+const convertToPdf = function (htmlUtf8, callback) {
+    const memStream = new MemoryStream();
+    wkhtmltopdf(htmlUtf8, {}, function (code, signal) {
+        console.log('code', code)
+
+        callback(memStream.read());
+    }).pipe(memStream);
+};
+
+async function preparePdfBinary() {
+    await fs.copySync('/var/task/wkhtmltopdf', '/tmp/wkhtmltopdf');
+    await fs.chmodSync('/tmp/wkhtmltopdf', 0o777)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const webpack = require('webpack');
+const CopyWebpacPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: slsw.lib.entries,
+    target: 'node',
     resolve: {
         extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
     },
@@ -11,10 +14,14 @@ module.exports = {
         path: path.join(__dirname, '.webpack'),
         filename: '[name].js'
     },
-    target: 'node',
+    plugins: [
+        new CopyWebpacPlugin([
+            {from: 'wkhtmltopdf'}
+        ])
+    ],
     module: {
         loaders: [
-            {test: /\.ts(x?)$/, loader: 'ts-loader'},
+            {test: /.ts$/, loader: 'ts-loader', exclude: [/node_modules/, '*.spec.ts']}
         ]
     }
 };
